d43341a1c9ab2a9512e1289b276e71e0596de7f8
syed umed asim <umed7095@gmail.com>
2025-12-19 23:27:18 +0530

    profileModel change

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -5,7 +5,7 @@ import Loading from "../components/Loading";
 import UserProfileInfo from "../components/UserProfileInfo";
 import PostCard from "../components/PostCard";
 import moment from "moment";
-import ProfileModel from "../components/profileModel";
+import ProfileModel from "./components/ProfileModel";
 
 const Profile = () => {
   const { profileId } = useParams();
